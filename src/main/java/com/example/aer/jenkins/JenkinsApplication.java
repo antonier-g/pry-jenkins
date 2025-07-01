d69c374308fb3a2affee52fe381cc23a2e0ad547
antonier-g <lianenra@gmail.com>
2025-06-30 23:32:13 -0500

    Proyecto springboot con pruebas unitarias para jenkins

--- src/main/java/com/example/aer/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/aer/jenkins/JenkinsApplication.java
@@ -8,7 +8,7 @@ public class JenkinsApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("xxxxxxxxxxxxxxxxxxx -- 11111222211111111111111111111111111111111111111");
+		System.out.println("6999xxxxxxxxxxxxxxxxxxx -- 11111222211111111111111111111111111111111111111");
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
